fb1187ee9430f9162ddbfb192d74d5c0a6f77af3
John Cris <johncrismanabo9@gmail.com>
2023-05-26 15:32:33 +0800

    display the anti rabies data

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -8,6 +8,8 @@ import {
   CCardFooter,
   CCardHeader,
   CCol,
+  CForm,
+  CFormSelect,
   CProgress,
   CRow,
 } from '@coreui/react'
@@ -45,9 +47,12 @@ const Dashboard = () => {
   const [email, setEmail] = useState(null)
   const [barangayOptions, setBarangayOptions] = useState([])
   const [petOwnerData, setPetOwnerData] = useState([])
+  const [selectedSpecies, setSelectedSpecies] = useState('C')
+  const [antiRabiesData, setAntiRabiesData] = useState([])
   useEffect(() => {
     fetchPetOwnerData()
 
+    fetchAntiRabiesData(selectedSpecies)
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         setEmail(user.email)
@@ -119,6 +124,64 @@ const Dashboard = () => {
     }
   }
 
+  const fetchAntiRabiesData = async (_species = 'C') => {
+    try {
+      const barangayRef = ref(database, 'barangay')
+      const barangaySnapshot = await get(barangayRef)
+      const barangays = Object.values(barangaySnapshot.val()).map((barangay) => barangay.barangay)
+
+      const antiRabiesRef = ref(database, 'anti_rabies_vaccination')
+      const antiRabiesSnapshot = await get(antiRabiesRef)
+      const antiRabies = Object.values(antiRabiesSnapshot.val())
+
+      const maleCounts = []
+      const femaleCounts = []
+      const currentYear = new Date().getFullYear()
+      // Loop through each anti rabies
+      for (const anti_rabies of antiRabies) {
+        const { address, sex, timestamp, species } = anti_rabies
+        if (currentYear == new Date(timestamp).getFullYear()) {
+          if (species == _species) {
+            // Find the corresponding barangay for the current anti rabies
+            const index = barangays.indexOf(address)
+            if (index !== -1) {
+              if (sex === 'Male') {
+                maleCounts[index] = (maleCounts[index] || 0) + 1
+              } else if (sex === 'Female') {
+                femaleCounts[index] = (femaleCounts[index] || 0) + 1
+              }
+            }
+          }
+        }
+      }
+
+      const _antiRabiesData = {
+        labels: barangays,
+        datasets: [
+          {
+            label: 'Male',
+            backgroundColor: '#f87979',
+            data: maleCounts,
+          },
+          {
+            label: 'Female',
+            backgroundColor: '#799ff8',
+            data: femaleCounts,
+          },
+        ],
+      }
+
+      setAntiRabiesData(_antiRabiesData)
+    } catch (error) {
+      console.error('Error fetching pet owner data:', error)
+    }
+  }
+
+  const handleAntiRabiesChange = (e) => {
+    const { value } = e.target
+    setSelectedSpecies(value)
+  }
+
   return (
     <>
       {status === 'Approved' ? (
@@ -129,12 +192,34 @@ const Dashboard = () => {
                 <CRow>
                   <CCol sm={12}>
                     <h4 id="pet-owner" className="card-title mb-0">
-                      Pet Owner
+                      Pet
                     </h4>
-                    {/* <div className="small text-medium-emphasis">January - July 2021</div> */}
                   </CCol>
                 </CRow>
                 <CChartBar data={petOwnerData} height={200} labels="pet_owner" />
+              </CCardBody>
+            </CCard>
+          </CCol>
+          <CCol md={6}>
+            <CCard className="mb-4">
+              <CCardBody>
+                <CRow>
+                  <CCol xs={5}>
+                    <h4 id="anti-rabies" className="card-title mb-0">
+                      Anti-Rabies
+                    </h4>
+                  </CCol>
+                  <CCol xs={7} className="d-none d-md-block">
+                    <CForm className="float-end">
+                      <CFormSelect size="sm" name="species" onChange={handleAntiRabiesChange}>
+                        <option disabled>Choose...</option>
+                        <option value="C">C - Iro</option>
+                        <option value="F">F - Iring</option>
+                      </CFormSelect>
+                    </CForm>
+                  </CCol>
+                </CRow>
+                <CChartBar data={antiRabiesData} height={200} labels="anti_tabies" />
               </CCardBody>
             </CCard>
           </CCol>
