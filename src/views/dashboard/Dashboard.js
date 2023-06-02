453b6fe417f29acb8b6925ff1bc7894bbb79bf35
John Cris <johncrismanabo9@gmail.com>
2023-06-02 10:20:51 +0800

    add filter by date in dog pound

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -8,8 +8,17 @@ import {
   CCardFooter,
   CCardHeader,
   CCol,
+  CDropdown,
+  CDropdownItem,
+  CDropdownMenu,
+  CDropdownToggle,
   CForm,
+  CFormInput,
   CFormSelect,
+  CModal,
+  CModalBody,
+  CModalHeader,
+  CModalTitle,
   CProgress,
   CRow,
 } from '@coreui/react'
@@ -40,23 +49,40 @@ import {
   onValue,
   remove,
 } from '../../firebaseConfig'
-import { cilCloudDownload } from '@coreui/icons'
-import { getStyle, hexToRgba } from '@coreui/utils'
-import { TimeScale } from 'chart.js'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import { cilCalendar, cilCalendarCheck, cilCloudDownload, cilOptions } from '@coreui/icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilter } from '@fortawesome/free-solid-svg-icons'
+
 const Dashboard = () => {
   const [status, setStatus] = useState(null)
   const [email, setEmail] = useState(null)
   const [dogPoundData, setDogPoundData] = useState([])
   const [dogPoundTotalData, setDogPoundTotalData] = useState([])
-  const [selectedSpeciesAntiRabies, setSelectedSpeciesAntiRabies] = useState('C')
+  const [speciesOptions, setSpeciesOptions] = useState([])
+  const [selectedSpeciesAntiRabies, setSelectedSpeciesAntiRabies] = useState('-NWjlZLSfWsrJgtMS0j7')
   const [antiRabiesData, setAntiRabiesData] = useState([])
   const [antiRabiesTotalData, setAntiRabiesTotalData] = useState([])
   const [selectedSpeciesDeworming, setSelectedSpeciesDeworming] = useState('Carabao')
   const [dewormingData, setDewormingData] = useState([])
   const [dewormingTotalData, setDewormingTotalData] = useState([])
+  const [startDate, setStartDate] = useState(new Date('2014/02/08'))
+  const [endDate, setEndDate] = useState(new Date('2014/02/10'))
+  const [dogPoundFormModalVisible, setDogPoundFormModalVisible] = useState(false)
+  const [validated, setValidated] = useState(false)
+
+  const [formDogPoundData, setFormDogPoundData] = useState({
+    start_date: '',
+    end_date: '',
+  })
+
   useEffect(() => {
-    fetchPetOwnerData()
+    fetchDogPoundData()
+    // Anti Rabies data
+    fetchAntiTabiesSpecies()
     fetchAntiRabiesData(selectedSpeciesAntiRabies)
+    // Deworming data
     fetchDewormingData(selectedSpeciesDeworming)
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
@@ -78,15 +104,20 @@ const Dashboard = () => {
 
     return () => unsubscribe()
   }, [dogPoundData])
-  const fetchPetOwnerData = async () => {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString)
+    const options = { month: 'long', day: 'numeric', year: 'numeric' }
+    return date.toLocaleDateString('en-US', options)
+  }
+  const fetchDogPoundData = async () => {
     try {
       const barangayRef = ref(database, 'barangay')
       const barangaySnapshot = await get(barangayRef)
       const barangays = Object.values(barangaySnapshot.val()).map((barangay) => barangay.barangay)
 
-      const petOwnersRef = ref(database, 'pet_owner')
-      const petOwnersSnapshot = await get(petOwnersRef)
-      const petOwners = Object.values(petOwnersSnapshot.val())
+      const dogPoundsRef = ref(database, 'dog_pound')
+      const dogPoundsSnapshot = await get(dogPoundsRef)
+      const dogPounds = Object.values(dogPoundsSnapshot.val())
 
       const maleCounts = []
       const femaleCounts = []
@@ -94,9 +125,20 @@ const Dashboard = () => {
       let totalFemaleCount = 0
       const currentYear = new Date().getFullYear()
       // Loop through each dog pound
-      for (const petOwner of petOwners) {
-        const { address, sex, timestamp } = petOwner
-        if (currentYear == new Date(timestamp).getFullYear()) {
+      for (const dogPound of dogPounds) {
+        const { address, sex, date } = dogPound
+        const dogPoundDate = new Date(date).getFullYear()
+        const startDate = formDogPoundData.start_date ? new Date(formDogPoundData.start_date) : null
+        const endDate = formDogPoundData.end_date ? new Date(formDogPoundData.end_date) : null
+        // console.info({ startDate, endDate })
+        if (
+          currentYear === dogPoundDate &&
+          (startDate === null || endDate === null
+            ? true
+            : new Date(date) >= startDate && new Date(date) < endDate)
+        ) {
+          let _date = new Date(date)
+          console.info({ startDate, endDate, _date })
           // Find the corresponding barangay for the current dog pound
           const index = barangays.indexOf(address)
           if (index !== -1) {
@@ -136,7 +178,6 @@ const Dashboard = () => {
       console.error('Error fetching dog owner data:', error)
     }
   }
-
   const fetchAntiRabiesData = async (_species) => {
     try {
       const barangayRef = ref(database, 'barangay')
@@ -265,9 +306,31 @@ const Dashboard = () => {
     setSelectedSpeciesAntiRabies(value)
   }
 
+  const fetchAntiTabiesSpecies = async () => {
+    try {
+      const databaseRef = ref(database, 'anti_rabies_species')
+      const snapshot = await get(databaseRef)
+      if (snapshot.exists()) {
+        const species = Object.values(snapshot.val()).sort((a, b) => a.name.localeCompare(b.name))
+        setSpeciesOptions(species)
+      }
+    } catch (error) {
+      console.error('Error fetching species data:', error)
+    }
+  }
+
   const handleDewormChange = (e) => {
     const { value } = e.target
     setSelectedSpeciesDeworming(value)
+  }
+
+  const handleDisplayDogPoundModal = () => {
+    setDogPoundFormModalVisible(true)
+  }
+
+  const handleDogPoundChange = (e) => {
+    const { name, value } = e.target
+    setFormDogPoundData({ ...formDogPoundData, [name]: value })
   }
 
   return (
@@ -278,7 +341,7 @@ const Dashboard = () => {
             <CCard className="mb-4">
               <CCardBody>
                 <CRow>
-                  <CCol md={12}>
+                  <CCol sm={5}>
                     <h4 id="dog-pound" className="card-title mb-0">
                       Dog Pound
                     </h4>
@@ -287,8 +350,18 @@ const Dashboard = () => {
                       <strong>Female:</strong> {dogPoundTotalData.female}
                     </div>
                   </CCol>
+                  <CCol sm={7} className="d-md-block">
+                    <CButton
+                      color="primary"
+                      variant="outline"
+                      className="float-end"
+                      onClick={handleDisplayDogPoundModal}
+                    >
+                      <CIcon icon={cilCalendar} />
+                    </CButton>
+                  </CCol>
                 </CRow>
-                <CChartBar height={150} data={dogPoundData} labels="pet_owner" />
+                <CChartBar height={150} data={dogPoundData} labels="dog-pound" />
               </CCardBody>
             </CCard>
           </CCol>
@@ -296,7 +369,7 @@ const Dashboard = () => {
             <CCard className="mb-4">
               <CCardBody>
                 <CRow>
-                  <CCol xs={5}>
+                  <CCol xs={4}>
                     <h4 id="anti-rabies" className="card-title mb-0">
                       Anti-Rabies
                     </h4>
@@ -305,12 +378,15 @@ const Dashboard = () => {
                       <strong>Female:</strong> {antiRabiesTotalData.female}
                     </div>
                   </CCol>
-                  <CCol xs={7} className="d-md-block">
+                  <CCol xs={8} className="text-end">
                     <CForm className="float-end">
                       <CFormSelect size="sm" name="species" onChange={handleAntiRabiesChange}>
                         <option disabled>Choose...</option>
-                        <option value="C">C - Iro</option>
-                        <option value="F">F - Iring</option>
+                        {speciesOptions.map((species) => (
+                          <option key={species.id} value={species.id}>
+                            {species.name}
+                          </option>
+                        ))}
                       </CFormSelect>
                     </CForm>
                   </CCol>
@@ -319,7 +395,7 @@ const Dashboard = () => {
               </CCardBody>
             </CCard>
           </CCol>
-          <CCol md={12}>
+          {/* <CCol md={12}>
             <CCard className="mb-4">
               <CCardBody>
                 <CRow>
@@ -346,7 +422,87 @@ const Dashboard = () => {
                 <CChartBar data={dewormingData} height={150} labels="anti_tabies" />
               </CCardBody>
             </CCard>
-          </CCol>
+          </CCol> */}
+
+          {/* Dog Pound Date Range */}
+          <CModal
+            alignment="center"
+            visible={dogPoundFormModalVisible}
+            onClose={() => setDogPoundFormModalVisible(false)}
+            backdrop="static"
+            keyboard={false}
+            size="md"
+          >
+            <CModalHeader>
+              <CModalTitle>
+                <FontAwesomeIcon icon={faFilter} /> Filter by Date
+              </CModalTitle>
+            </CModalHeader>
+            <CModalBody>
+              <p className="text-small-emphasis">
+                Note:{' '}
+                <strong>
+                  <span className="text-danger">*</span> is required
+                </strong>
+              </p>
+              <CForm className="row g-3 needs-validation" noValidate validated={validated}>
+                <CCol md={6}>
+                  <label htmlFor="startDate">
+                    {
+                      <>
+                        Start Date
+                        <span className="text-warning">
+                          <strong>*</strong>
+                        </span>
+                      </>
+                    }
+                  </label>
+                  <DatePicker
+                    selected={formDogPoundData.start_date}
+                    className="form-control"
+                    onChange={(date) =>
+                      handleDogPoundChange({ target: { name: 'start_date', value: date } })
+                    }
+                    selectsStart
+                    startDate={formDogPoundData.start_date}
+                    endDate={formDogPoundData.end_date}
+                    name="start_date"
+                    required
+                  />
+                </CCol>
+                <CCol md={6}>
+                  <label htmlFor="endDate">
+                    {
+                      <>
+                        End Date
+                        <span className="text-warning">
+                          <strong>*</strong>
+                        </span>
+                      </>
+                    }
+                  </label>
+                  <DatePicker
+                    className="form-control"
+                    selected={formDogPoundData.end_date}
+                    onChange={(date) =>
+                      handleDogPoundChange({ target: { name: 'end_date', value: date } })
+                    }
+                    selectsEnd
+                    startDate={formDogPoundData.start_date}
+                    endDate={formDogPoundData.end_date}
+                    minDate={formDogPoundData.start_date}
+                    name="end_date"
+                    required
+                  />
+                </CCol>
+                {/* <CCol xs={12}>
+                  <CButton color="primary" type="submit" className="float-end">
+                    Generate
+                  </CButton>
+                </CCol> */}
+              </CForm>
+            </CModalBody>
+          </CModal>
         </CRow>
       ) : (
         <CRow>
